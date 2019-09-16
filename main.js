4ea17574e262873e17e40cea03f15a413d8d8bd2
Michele Tagliani <giantaglio@gmail.com>
2019-09-16 18:43:46 +0200

    Add color when u are losig money

--- main.js
+++ main.js
@@ -6,7 +6,9 @@ const BASE_URI="https://api.worldtradingdata.com/api/v1/stock?symbol="
 //https://api.worldtradingdata.com/api/v1/stock?symbol=AAPL,MSFT,HSBA.L&api_token=tMOgm8UlkcmqcOpPbI27kFGMy2BMLPeWZ4zwyh33UzJRCKFZKZ8U9pkSjpx6
 
 let infos = []
-
+let data = []
+let i = 0;
+let c = 0;
 
 function getDataUri()
 {
@@ -24,12 +26,31 @@ fetch(getDataUri())
              value: q.price,
              max: q.day_high,
              min: q.day_low,
-             earn: q.day_change, 
+             earn: q.day_change,
           }
           infos.push(info)
+          i++;
         })
-        console.log(infos)
+
+//
+          document.getElementById("1").innerHTML = infos[0].name + ": " + infos[0].value;
+          if (infos[0].earn >0){
+            document.getElementById("1").style.backgroundColor = "lightgreen";
+          }
+          else {
+            document.getElementById("1").style.backgroundColor = "lightred";
+          }
+
+          document.getElementById("2").innerHTML = infos[1].name + ": " + infos[1].value;
+          //document.getElementById("2").innerHTML = infos[1].name +  ": " + infos[1].value;
+          if (infos[1].earn >1){
+            document.getElementById("2").style.backgroundColor = "lightgreen";
+          }
+          else {
+            document.getElementById("2").style.backgroundColor = "red";
+          }
 
 
-
+        console.log(infos[1])
+        console.log(i)
       })
