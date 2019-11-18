6de421faa26743244980be772ff5fd23ca459858
Michele Tagliani <giantaglio@gmail.com>
2019-11-18 11:14:18 +0100

    First graph

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 console.log("Start")
 
-const APP_ID="tMOgm8UlkcmqcOpPbI27kFGMy2BMLPeWZ4zwyh33UzJRCKFZKZ8U9pkSjpx6"
-const BASE_URI="https://api.worldtradingdata.com/api/v1/stock?symbol="
+const APP_ID = "tMOgm8UlkcmqcOpPbI27kFGMy2BMLPeWZ4zwyh33UzJRCKFZKZ8U9pkSjpx6"
+const BASE_URI = "https://api.worldtradingdata.com/api/v1/stock?symbol="
 
 //https://api.worldtradingdata.com/api/v1/stock?symbol=AAPL,MSFT,HSBA.L&api_token=tMOgm8UlkcmqcOpPbI27kFGMy2BMLPeWZ4zwyh33UzJRCKFZKZ8U9pkSjpx6
 
@@ -14,66 +14,94 @@ let date;
 let dateObj = new Date();
 let time;
 
-function getDataUri()
-{
+
+function getDataUri() {
   return `${BASE_URI}AAPL,RACE.MI&api_token=${APP_ID}`
 }
 
-function getStoricUri(){
-  dateObj.setMonth(dateObj.getMonth()-1)
+function getStoricUri() {
+  dateObj.setMonth(dateObj.getMonth() - 1)
   date = dateObj.toISOString()
-  date = date.substring(0,10)
+  date = date.substring(0, 10)
   return `https://api.worldtradingdata.com/api/v1/history?symbol=AAPL&sort=newest&date_from=${date}&api_token=${APP_ID}`
 }
 
+function addAll() {
+  document.getElementById("1").innerHTML = infos[0].name + ": " + infos[0].value;
+  if (infos[0].earn > 0) {
+    document.getElementById("1").style.backgroundColor = "lightgreen";
+  } else {
+    document.getElementById("1").style.backgroundColor = "red";
+  }
+
+  document.getElementById("2").innerHTML = infos[1].name + ": " + infos[1].value;
+  //document.getElementById("2").innerHTML = infos[1].name +  ": " + infos[1].value;
+  if (infos[1].earn > 0) {
+    document.getElementById("2").style.backgroundColor = "lightgreen";
+  } else {
+    document.getElementById("2").style.backgroundColor = "red";
+  }
+}
+
 fetch(getDataUri())
-      .then((response) => response.json())
-      .then((body) =>{
-      //  console.log(body.data)
+  .then((response) => response.json())
+  .then((body) => {
+    console.log(body.data)
 
-        body.data.forEach(q => {
-          let info = {
-             name: q.name,
-             value: q.price,
-             max: q.day_high,
-             min: q.day_low,
-             earn: q.day_change,
+    body.data.forEach(q => {
+      let info = {
+        name: q.name,
+        value: q.price,
+        max: q.day_high,
+        min: q.day_low,
+        earn: q.day_change,
+      }
+      infos.push(info)
+      i++;
+    })
+
+    addAll()
+
+  })
+
+
+
+fetch(getStoricUri())
+  .then((response) => response.json())
+  .then((item) => {
+
+
+     dayPrice = Object.keys(item.history).map(function(key) {
+      return {day: key, data: item.history[key]};
+    });
+  })
+  .then((item)=>{
+
+    console.log(dayPrice)
+
+    var dates  = [dayPrice[2].day, dayPrice[1].day, dayPrice[0].day]
+    var price = [dayPrice[2].data.close , dayPrice[1].data.close , dayPrice[0].data.close]
+    console.log(price)
+
+    //grafico apple
+    var ctx = document.getElementById('one');
+    var myLineChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: dates,
+      //x
+      datasets:[
+        {
+          borderColor: 'rgba(0,0,255,1)',
+          backgroundColor: 'rgba(135,206,250, 0.2)',
+          label: 'Price',
+          data: price,//y
+          datalabels: { //ombrellini più in alto
+            align: 'end',
+            anchor: 'end'
           }
-          infos.push(info)
-          i++;
-        })
-
-//
-          document.getElementById("1").innerHTML = infos[0].name + ": " + infos[0].value;
-          if (infos[0].earn > 0){
-            document.getElementById("1").style.backgroundColor = "lightgreen";
-          }
-          else {
-            document.getElementById("1").style.backgroundColor = "red";
-          }
-
-          document.getElementById("2").innerHTML = infos[1].name + ": " + infos[1].value;
-          //document.getElementById("2").innerHTML = infos[1].name +  ": " + infos[1].value;
-          if (infos[1].earn > 0){
-            document.getElementById("2").style.backgroundColor = "lightgreen";
-          }
-          else {
-            document.getElementById("2").style.backgroundColor = "red";
-          }
-
-
-          fetch(getStoricUri())
-                .then((response) => response.json())
-                .then((item) =>{
-
-
-                  let test = Object.keys(item.history).map(function(key) {
-                      return [key, item.history[key]];
-                    });
-
-                    console.log(test)
-
-                  
-                })
-
-      })
+        }
+      ]
+    }
+  });//grafico
+})
