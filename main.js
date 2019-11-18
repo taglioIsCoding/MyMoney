7a919ddbfcbce4d65ef66d8adc63a357ed72bc07
Michele Tagliani <giantaglio@gmail.com>
2019-11-18 16:00:25 +0100

    Add all chart

--- main.js
+++ main.js
@@ -26,6 +26,14 @@ function getStoricUri() {
   return `https://api.worldtradingdata.com/api/v1/history?symbol=AAPL&sort=newest&date_from=${date}&api_token=${APP_ID}`
 }
 
+function getStoricUriF(){
+  dateObj.setMonth(dateObj.getMonth())
+  date = dateObj.toISOString()
+  date = date.substring(0, 10)
+  console.log(date)
+  return `https://api.worldtradingdata.com/api/v1/history?symbol=RACE.MI&sort=newest&date_from=${date}&api_token=${APP_ID}`
+}
+
 function addAll() {
   document.getElementById("1").innerHTML = infos[0].name + ": " + infos[0].value;
   if (infos[0].earn > 0) {
@@ -42,6 +50,7 @@ function addAll() {
     document.getElementById("2").style.backgroundColor = "red";
   }
 }
+
 
 fetch(getDataUri())
   .then((response) => response.json())
@@ -65,7 +74,7 @@ fetch(getDataUri())
   })
 
 
-
+//STORICO APPLE
 fetch(getStoricUri())
   .then((response) => response.json())
   .then((item) => {
@@ -77,26 +86,81 @@ fetch(getStoricUri())
   })
   .then((item)=>{
 
-    console.log(dayPrice)
-
-    var dates  = [dayPrice[2].day, dayPrice[1].day, dayPrice[0].day]
-    var price = [dayPrice[2].data.close , dayPrice[1].data.close , dayPrice[0].data.close]
-    console.log(price)
+    var dates = []
+    dayPrice.reverse()
+    dayPrice.forEach(d => {
+        dates.push(d.day.slice(4-9))
+    })
+    var prices = []
+    dayPrice.forEach(d => {
+        prices.push(d.data.close)
+    })
 
     //grafico apple
     var ctx = document.getElementById('one');
     var myLineChart = new Chart(ctx, {
     type: 'line',
+
     data: {
       labels: dates,
       //x
       datasets:[
         {
+          maintainAspectRatio: false,
           borderColor: 'rgba(0,0,255,1)',
           backgroundColor: 'rgba(135,206,250, 0.2)',
           label: 'Price',
-          data: price,//y
-          datalabels: { //ombrellini più in alto
+          lineTension: 0,
+          data: prices,//y
+          datalabels: { //valore
+            align: 'end',
+            anchor: 'end'
+          }
+        }
+      ]
+    }
+  });//grafico
+})
+
+//STORICO FERRARI
+fetch(getStoricUriF())
+  .then((response) => response.json())
+  .then((item) => {
+
+
+     dayPrice = Object.keys(item.history).map(function(key) {
+      return {day: key, data: item.history[key]};
+    });
+  })
+  .then((item)=>{
+
+    var dates = []
+    dayPrice.reverse()
+    dayPrice.forEach(d => {
+        dates.push(d.day.slice(4-9))
+    })
+    var prices = []
+    dayPrice.forEach(d => {
+        prices.push(d.data.close)
+    })
+
+    //grafico apple
+    var ctx = document.getElementById('two');
+    var myLineChart = new Chart(ctx, {
+    type: 'line',
+
+    data: {
+      labels: dates,
+      //x
+      datasets:[
+        {
+          maintainAspectRatio: false,
+          borderColor: 'rgb(255,127,80, 1)',
+          backgroundColor: 'rgb(255,165,0, 0.2)',
+          label: 'Price',
+          lineTension: 0,
+          data: prices,//y
+          datalabels: { //valore
             align: 'end',
             anchor: 'end'
           }
